c029ef51f04cd0530dd573d8ade57959ed0d3c18
Stanislav Karlov <karlovvski@gmail.com>
2023-10-03 15:56:13 +0300

    added: mongoose + nest config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,8 +8,17 @@ import { AuthModule } from './api/auth/auth.module';
 import { ProjectsModule } from './api/projects/projects.module';
 import { TasksModule } from './api/tasks/tasks.module';
 
+
 @Module({
-  imports: [AuthModule, ProjectsModule, TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URI, { dbName: process.env.MONGODB_NAME }),
+    AuthModule, 
+    ProjectsModule, 
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
