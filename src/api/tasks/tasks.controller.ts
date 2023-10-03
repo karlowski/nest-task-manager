f57c2ff50002ea2e1aee7d43eae263c7440280d6
Stanislav Karlov <karlovvski@gmail.com>
2023-10-03 23:56:34 +0300

    changed: api response types

--- src/api/tasks/tasks.controller.ts
+++ src/api/tasks/tasks.controller.ts
@@ -4,7 +4,7 @@ import { TasksService } from './tasks.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { CreateTaskDto } from 'src/dto/create-task.dto';
 import { ITask } from 'src/interfaces/task.interface';
-import { TaskOperation } from 'src/interfaces/task-operation.interface';
+import { ApiOperation } from 'src/interfaces/api-operation.interface';
 import { UpdateTaskDto } from 'src/dto/update-task.dto';
 import { TaskResponse } from 'src/interfaces/task-response.interface';
 
@@ -24,17 +24,17 @@ export class TasksController {
   }
 
   @Post()
-  create(@Body() task: CreateTaskDto): Promise<TaskOperation<TaskResponse>> {
+  create(@Body() task: CreateTaskDto): Promise<ApiOperation<TaskResponse>> {
     return this.tasksService.create(task);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() task: UpdateTaskDto): Promise<TaskOperation<TaskResponse>> {
+  update(@Param('id') id: string, @Body() task: UpdateTaskDto): Promise<ApiOperation<TaskResponse>> {
     return this.tasksService.update(id, task);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): Promise<any> {
+  delete(@Param('id') id: string): Promise<ApiOperation<TaskResponse>> {
     return this.tasksService.delete(id);
   }
 
